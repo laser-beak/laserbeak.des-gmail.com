5f39433f43c3c2d60e8c64224e63d680cca002d8
laser-beak <laserbeak.des@gmail.com>
2020-04-09 20:11:41 +0300

    Fill project code

---
+++ src/main/java/ru/aincorp/smartbytes/SmartBytesDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.aincorp.smartbytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBytesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBytesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/aincorp/smartbytes/SmartBytesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aincorp.smartbytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBytesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
